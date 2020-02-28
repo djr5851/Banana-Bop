9edc83e6e07138ed0a3ec940600759b84310df9f
djr5851 <42845228+djr5851@users.noreply.github.com>
2020-02-28 15:59:24 -0500

    2/28
    - Tree with shadows
    - WIP mouse/keyboard controlled camera, press q to toggle

---
+++ Assets/Easy Grab VR/Demo/Scripts/SwitchCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCamera : MonoBehaviour
+{
+    public Camera vrCam;
+    public Camera mouseCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            vrCam.enabled = !vrCam.enabled;
+            mouseCam.enabled = !mouseCam.enabled;
+        }
+    }
+}
