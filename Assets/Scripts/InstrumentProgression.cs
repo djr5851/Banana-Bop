d1cc0a07357586e54d16a4fea282b70e11ba3dde
Dan Robinson <djr5851@rit.edu>
2020-04-10 12:00:56 -0400

    instrument progression

---
+++ Assets/Scripts/InstrumentProgression.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstrumentProgression : MonoBehaviour
+{
+    public GameObject stick;
+    public GameObject rock;
+    public GameObject mallet;
+    public GameObject xylophone;
+    public float timer;
+    public float rockStartTime;
+    public float xylophoneStartTime;
+
+    void Start()
+    {
+        timer = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= rockStartTime)
+        {
+            stick.SetActive(true);
+            rock.SetActive(true);
+        }
+        if (timer >= xylophoneStartTime)
+        {
+            mallet.SetActive(true);
+            xylophone.SetActive(true);
+        }
+    }
+}
