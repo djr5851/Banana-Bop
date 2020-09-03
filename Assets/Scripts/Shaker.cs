d595607b4fa1d8d241fded6789f368d4708e4e4f
djr5851 <42845228+djr5851@users.noreply.github.com>
2020-09-03 16:10:37 -0400

    Added back VR
    - organized a lil bit
    - VR grabbing works
    - shaker started

--- Assets/Scripts/Shaker.cs
+++ Assets/Scripts/Shaker.cs
@@ -4,27 +4,33 @@ using UnityEngine;
 
 public class Shaker : MonoBehaviour
 {
-    public bool speedExceeded;
+    public bool veloityExceeded;
+    public float velocity;
+    private Vector3 previous;
     // Start is called before the first frame update
     void Start()
     {
-        speedExceeded = false;
+        previous = transform.position;
+        veloityExceeded = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!speedExceeded)
+        velocity = ((transform.position - previous).magnitude) / Time.deltaTime;
+        previous = transform.position;
+        if (!veloityExceeded)
         {
-            if (GetComponent<Rigidbody>().velocity.sqrMagnitude > 50)
-                speedExceeded = true;
+            if (velocity > 50)
+                veloityExceeded = true;
         }
         else
         {
-            if (GetComponent<Rigidbody>().velocity.sqrMagnitude <= 1)
+            if (velocity <= 1)
             {
                 GetComponent<AudioSource>().Play();
-                speedExceeded = false;
+                veloityExceeded = false;
             }
         }
     }
