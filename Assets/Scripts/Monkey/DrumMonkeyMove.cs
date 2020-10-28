415f1eaa0271ead057d12f1d80da0ef0614f74b4
djr5851 <42845228+djr5851@users.noreply.github.com>
2020-10-27 20:02:54 -0400

    sonkey push in drum

---
+++ Assets/Scripts/Monkey/DrumMonkeyMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrumMonkeyMove : MonoBehaviour
+{
+    public float speed;
+    public GameObject monky;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        gameObject.transform.Translate(-speed, 0, 0);
+        if(gameObject.transform.position.x <= 0.86f && gameObject.transform.position.z <= 2.91f)
+        {
+            monky.GetComponent<Animator>().SetTrigger("dance");
+            this.enabled = false;
+        }
+    }
+}
